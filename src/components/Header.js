bd7aff8ced7b52c1680a468a29d7270375df7d04
ronit <RonitEcosmob>
2021-09-29 10:37:32 +0530

    Adding Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,20 +10,20 @@ const mycolor = "#212121"
 
   return (
     <View style={{
-        margin:Constant.statusBarHeight,
+        marginTop:Constant.statusBarHeight,
         height:45,
         background:"white",
         flexDirection:"row",
         justifyContent:"space-between",
         elevation:4,
-        shadowOffset:{width:10,height:10,},
+        shadowOffset:{width:10,height:10},
         shadowColor:'black',
         shadowOpacity:1.0
     }}>
 
       <View style={{flexDirection:"row", margin:5}}>
         <AntDesign style={{
-            marginLeft:20
+            marginLeft:10
         }} name="youtube" size={32} color="red" />
         <Text style={{
             fontSize:22,
@@ -33,7 +33,7 @@ const mycolor = "#212121"
         }}>YouTube</Text>
       </View>
 
-      <View style={{flexDirection:"row", justifyContent:"space-around" width:150, margin:5}} >
+      <View style={{flexDirection:"row", justifyContent:"space-around", width:150, margin:5}} >
         <Ionicons name="md-videocam" size={32} color={mycolor} />
         <Ionicons name="md-search" size={32} color={mycolor} />
         <MaterialIcons name="account-circle" size={32} color={mycolor} />
