909f85ef559e87ea26765c78dfbd371f0e26fd0b
ronit <RonitEcosmob>
2021-09-29 17:21:45 +0530

    Fixing video images issue

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -6,7 +6,7 @@ const MiniCard = (props)=>{
     return(
         <View style={{flexDirection:"row", margin:10, marginBottom:0}}>
             <Image 
-                source={{uri:`https://i.ytimg.com/vi/${props.videoId}/maxresdefault.jpg`}}
+                source={{uri:`https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`}}
                 style={{
                     width:"45%",
                     height:100
