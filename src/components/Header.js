b9967f5988abbb6310cfb887b2d0c83fdc74ac4e
ronit <RonitEcosmob>
2021-10-05 17:25:50 +0530

    Add Scroll Animation to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {AntDesign, Ionicons, MaterialIcons} from '@expo/vector-icons'
-import { useNavigation, useTheme } from '@react-navigation/native';
-import Constant from 'expo-constants'
-import {useDispatch, useSelector} from 'react-redux'
+import React from "react";
+import { Text, View } from "react-native";
+import { AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { useNavigation, useTheme } from "@react-navigation/native";
+import Constant from "expo-constants";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Header() {
-
   const navigation = useNavigation();
-  const dispatch = useDispatch()
-  const {colors} = useTheme() 
-  const currentTheme = useSelector(state=>{
-      return state.myDarkMode
-  })
-  const mycolor = colors.iconColor
+  const dispatch = useDispatch();
+  const { colors } = useTheme();
+  const currentTheme = useSelector((state) => {
+    return state.myDarkMode;
+  });
+  const mycolor = colors.iconColor;
 
   return (
-    <View style={{
-        marginTop:Constant.statusBarHeight,
-        height:45,
-        backgroundColor:colors.headerColor,
-        flexDirection:"row",
-        justifyContent:"space-between",
-        elevation:4,
-        shadowOffset:{width:10,height:10},
-        shadowColor:'black',
-        shadowOpacity:1.0
-    }}>
-
-      <View style={{flexDirection:"row", margin:5}}>
-        <AntDesign style={{
-            marginLeft:10
-        }} name="youtube" size={32} color="red" />
-        <Text style={{
-            fontSize:22,
-            marginLeft:5,
-            color:mycolor,
-            fontWeight:"bold"
-        }}>YouTube</Text>
-      </View>
-
-      <View style={{flexDirection:"row", justifyContent:"space-around", width:150, margin:5}} >
-        <Ionicons name="md-videocam" size={32} color={mycolor} />
-        <Ionicons name="md-search" size={32} color={mycolor} 
-            onPress={()=> navigation.navigate("search")}
+    <View
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 45,
+        backgroundColor: colors.headerColor,
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <View style={{ flexDirection: "row", margin: 5 }}>
+        <AntDesign
+          style={{
+            marginLeft: 10,
+          }}
+          name="youtube"
+          size={32}
+          color="red"
         />
-        <MaterialIcons name="account-circle" size={32} color={mycolor}
-          onPress={()=>dispatch({type:"change_theme", payload:!currentTheme})}
-         />
+        <Text
+          style={{
+            fontSize: 22,
+            marginLeft: 5,
+            color: mycolor,
+            fontWeight: "bold",
+          }}
+        >
+          YouTube
+        </Text>
       </View>
 
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: 150,
+          margin: 5,
+        }}
+      >
+        <Ionicons name="md-videocam" size={32} color={mycolor} />
+        <Ionicons
+          name="md-search"
+          size={32}
+          color={mycolor}
+          onPress={() => navigation.navigate("search")}
+        />
+        <MaterialIcons
+          name="account-circle"
+          size={32}
+          color={mycolor}
+          onPress={() =>
+            dispatch({ type: "change_theme", payload: !currentTheme })
+          }
+        />
+      </View>
     </View>
   );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
-import { useSelector } from 'react-redux'
-import Header from '../components/Header'
-import Card from '../components/Card'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Animated,
+} from "react-native";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
+import Card from "../components/Card";
 
-export default function HomeScreen({navigation}) {
+// const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+// const HEADER_MAX_HEIGHT = 45;
+// const HEADER_MIN_HEIGHT = 0;
 
-  const cardData = useSelector(state=>{
-    return state.cardData
-  })
+export default function HomeScreen({ navigation }) {
+  // state = {
+  //   animatedValue: new Animated.Value(0),
+  // };
+
+  const scrollY = new Animated.Value(0);
+  const diffClamp = Animated.diffClamp(scrollY, 0, 45);
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, 45],
+    outputRange: [0, -45],
+  });
+
+  const cardData = useSelector((state) => {
+    return state.cardData;
+  });
+
+  // const height = state.animatedValue.interpolate({
+  //   inputRange: [0, HEADER_MAX_HEIGHT],
+  //   outputRange: [0, -HEADER_MIN_HEIGHT],
+  //   extrapolateRight: "clamp",
+  // });
+
+  // const onScrollEvent = Animated.event(
+  //   [{ nativeEvent: { contentOffset: { y: state.animatedValue } } }],
+  //   { useNativeDriver: true } // <-- Add this
+  // );
 
   return (
-    <View style={{flex:1}} >
-      
-      <Header />
-      <FlatList 
+    <View style={{ flex: 1 }}>
+      <Animated.View
+        style={{
+          transform: [{ translateY: translateY }],
+          elevation: 4,
+          zIndex: 100,
+        }}
+      >
+        <Header />
+      </Animated.View>
+
+      <FlatList
         data={cardData}
-        renderItem={({item})=>{
-          return <Card 
+        renderItem={({ item }) => {
+          return (
+            <Card
               videoId={item.id.videoId}
               title={item.snippet.title}
               channel={item.snippet.channelTitle}
-          />
+            />
+          );
         }}
-        keyExtractor={item=>item.id.videoId}
+        keyExtractor={(item) => item.id.videoId}
+        scrollEventThrottle={16}
+        // onScroll={onScrollEvent}
+        onScroll={(e) => {
+          scrollY.setValue(e.nativeEvent.contentOffset.y);
+        }}
       />
-      
     </View>
   );
 }
-
-
